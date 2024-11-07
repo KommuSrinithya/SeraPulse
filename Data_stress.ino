cd320201097635a34051096a94b1e814a1274c1f
KommuSrinithya <101787569+KommuSrinithya@users.noreply.github.com>
2024-11-07 19:14:06 +0530

    Add files via upload

---
+++ Data_stress.ino
@@ -0,0 +1,43 @@
+boolean countStatus;
+int beat, bpm;
+unsigned long millisBefore;
+ 
+// the setup routine runs once when you press reset:
+void setup()
+{
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+ 
+// the loop routine runs over and over again forever:
+void loop()
+{
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  //Serial.println(sensorValue);
+  if (countStatus == 0)
+  {
+    if (sensorValue > 600)
+    {
+      countStatus = 1;
+      beat++;
+    }
+  }
+  else
+  {
+    if (sensorValue < 500)
+    {
+      countStatus = 0;
+    }
+  }
+  if (millis() - millisBefore > 15000)
+  {
+    bpm = beat * 4;
+    beat = 0;
+    Serial.print("BPM : ");
+    Serial.println(bpm);
+    millisBefore = millis();
+  }
+  delay(1);        // delay in between reads for stability
+}
